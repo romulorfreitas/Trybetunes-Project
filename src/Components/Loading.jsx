5d85407a7db989803463388ebaff53332ffc5d20
Rômulo Freitas <110496787+romulorfreitas@users.noreply.github.com>
2022-11-03 22:17:20 -0400

    Entrega requisito 02

---
+++ src/Components/Loading.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Loading extends Component {
+  render() {
+    return (
+      <div>
+        <span className="loading">Carregando...</span>
+      </div>
+    );
+  }
+}
+
+export default Loading;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,87 @@
 import React, { Component } from 'react';
+import Props from 'prop-types';
+import { createUser } from '../services/userAPI';
+import Loading from '../Components/Loading';
+
+const NAME_LIMIT_LENGTH = 3;
+const THREE_SECONDS = 3000;
 
 class Login extends Component {
+  state = {
+    name: '',
+    isItLoading: false,
+    buttonDisable: true,
+  };
+
+  handleChange = ({ target }) => {
+    const { value } = target;
+    this.setState({
+      name: value,
+    });
+    if (value.length >= NAME_LIMIT_LENGTH) {
+      this.setState({
+        buttonDisable: false,
+      });
+    } else {
+      this.setState({
+        buttonDisable: true,
+      });
+    }
+  };
+
+  handleClickSubmit = (event) => {
+    event.preventDefault();
+    this.setState({ isItLoading: true });
+    const { history } = this.props;
+    const { name } = this.state;
+    createUser({ name });
+    setInterval(() => {
+      history.push('/search');
+    }, THREE_SECONDS);
+  };
+
   render() {
+    const { name, isItLoading, buttonDisable } = this.state;
     return (
-      <div data-testid="page-login">Login</div>
+
+      <div data-testid="page-login">
+
+        {
+          isItLoading ? <Loading /> : (
+
+            <form>
+              <label htmlFor="login-name-input">
+                Name:
+                <input
+                  type="text"
+                  data-testid="login-name-input"
+                  id="login-name-input"
+                  value={ name }
+                  onChange={ this.handleChange }
+                />
+              </label>
+              <button
+                type="submit"
+                data-testid="login-submit-button"
+                disabled={ buttonDisable }
+                onClick={ this.handleClickSubmit }
+              >
+                Entrar
+              </button>
+
+            </form>
+          )
+        }
+      </div>
+
     );
   }
 }
+
+Login.propTypes = {
+  history: Props.shape({
+    push: Props.func.isRequired,
+  }).isRequired,
+};
+
 export default Login;

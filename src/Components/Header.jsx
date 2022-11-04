1d2f15b3a4be9e0b2e70772b937d5cd5d0705377
Rômulo Freitas <110496787+romulorfreitas@users.noreply.github.com>
2022-11-04 16:58:42 -0400

    Entrega requisito 04

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -23,6 +24,11 @@ class Header extends Component {
         {isItLoading ? <Loading /> : (
           <p data-testid="header-user-name">{userInfo}</p>
         )}
+        <Link to="/search" data-testid="link-to-search">Search</Link>
+        <br />
+        <Link to="/favorites" data-testid="link-to-favorites">Favorites</Link>
+        <br />
+        <Link to="/profile" data-testid="link-to-profile">Profile</Link>
       </header>
     );
   }
